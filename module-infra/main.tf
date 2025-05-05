e10b56fbf13e15ce70bc2bc9e17549ecb90d5b57
vinay <vinaybkrishna10@gmail.com>
2025-05-05 16:10:16 +0530

    Commmmiting mmy first commit

--- module-infra/main.tf
+++ module-infra/main.tf
@@ -42,6 +42,14 @@ resource "aws_instance" "tool" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.tools.id]
 
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type = "persistent"
+    }
+  }
+
   tags = {
     Name = " ${ var.name }-terraform "
   }
